df921d2e80e6a599ad3d2e2dd92ed9232c8d762d
davidlaubser <davidlaubser12246@gmail.com>
2025-02-13 12:02:42 +0200

    Fix backend API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an axios instance with a base URL for the iTunes API
 const API = axios.create({
-    baseURL: 'https://itunes-app-u5dm.onrender.com'
+    baseURL: 'https://itunes-app-u5dm.onrender.com/api'
 });
 
 // Export the API instance
